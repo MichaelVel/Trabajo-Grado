8448a3403649de166c21ab06409684c0636a001f
MichaelVel <msvelandiag@unal.edu.co>
2021-05-01 23:52:51 -0500

    second commit

--- Code/metodos.R
+++ Code/metodos.R
@@ -48,14 +48,12 @@ load_data <- function(data, username, ...) {
 }
 
 
-mk_plot <- function(data, data2, muni = NULL, muni2 = NA,  bbox = NULL, expand = FALSE, 
+mk_plot <- function(data, data2, bbox = NULL, expand = FALSE, 
                     resolution = 200, ...){
 
-    data <- subset(data, Municipio == muni | Municipio == muni2)
-    
     if (expand == FALSE) { 
         return(prettymap({
-        osm.plot(bbox, res = resolution);
+        osm.plot(bbox, res = resolution, stoponlargerequest=FALSE);
         osm.points(data$Longitud, data$Latitud, pch = 18, cex = 1.5, 
                    col = "red");
         osm.text(data$Longitud, data$Latitud  , 
@@ -64,38 +62,44 @@ mk_plot <- function(data, data2, muni = NULL, muni2 = NA,  bbox = NULL, expand =
     }
     else {
         return(prettymap({
-        osm.plot(bbox, res = resolution);
+        osm.plot(bbox, res = resolution, stoponlargerequest=FALSE);
         osm.points(data$Longitud, data$Latitud, pch = 18, cex = 1.5, col = "red");
-        osm.points(data2$Longitud, data2$Latitud, pch = 3, cex = 1.5, col = "blue");
+        osm.text(data$Longitud, data$Latitud, 
+                 data$Estacion, cex = 1, col = "red", pos = 1);
+        osm.points(data2$Longitud, data2$Latitud, pch = 18, cex = 1.5, col = "blue");
         osm.text(data2$Longitud, data2$Latitud  , 
                  data2$Estacion, cex = 1, col = "blue", pos = 3);
         }))
     }
 }
 
-
-inventario <- load_data("raw_data/geografico.csv", user = "msvelandiag")
-est_neusa <- load_data("rio_neusa.csv", user = "msvelandiag")
-est_frio <- read.csv("transform_data/rio_frio.csv")
-
-#png("calera.png")
-mk_plot(inventario, muni = "La Calera", 
-        bbox = makebbox(w = -73.9787, s =4.8523, e = -73.9617, n = 4.7252)) 
-#dev.off()
-
-#png("cajica.png")
-mk_plot(inventario, est_frio, muni = "Cajica", muni2 = "Chia", expand = TRUE, resolution = 220,
-        bbox = makebbox(w = -74.1531, s =4.8389, e = -73.9787, n = 4.9346) ) 
-#dev.off()
-
-#png("lenguezaque.png")
-mk_plot(inventario, muni = "Lenguezaque",
-        bbox = makebbox(w = -73.7241, s =5.2740, e = -73.7002, n = 5.3001)) 
-#dev.off()
-#
-png("cogua.png", width= 1000, height = 1000)
-mk_plot(data = inventario, data2 = est_neusa, 
-       muni = "Cogua", expand = TRUE, resolution = 150,
-       bbox = makebbox(w = -74.0060, s =5.0728, e = -73.9291, n = 5.1670)) 
-dev.off()
-
+plot_est <-  function(device = 'rmarkdown' ) {
+    
+    est_neusa <- read.csv("Data/rio_neusa.csv")
+    est_frio <- read.csv("Data/rio_frio.csv")
+    
+    if (device == 'png'){ 
+        
+        png(filename = 'Results/rio_frio.png')
+        mk_plot(est_frio, expand = FALSE, resolution = 150,
+                bbox = makebbox(w = -74.1531, s =4.8389, e = -73.9787, n = 4.9346)) 
+        dev.off()
+        
+        png(filename = 'Results/rio_neusa.png')
+        mk_plot(est_neusa, expand = FALSE, resolution = 150,
+                bbox = makebbox(w = -74.0060, s =5.0728, e = -73.9291, n = 5.1670)) 
+        dev.off()
+    } else if (device == 'rmarkdown') {
+        par(fig=c(0,0.5,0,1))
+        mk_plot(est_frio, expand = FALSE, resolution = 150,
+                bbox = makebbox(w = -74.0863, s =4.8484, e = -74.0396, n = 4.9354)) 
+        par(fig=c(0.5,1,0,1), new=TRUE)
+        mk_plot(est_neusa, expand = FALSE, resolution = 150,
+                bbox = makebbox(w = -74.0060, s =5.0728, e = -73.9291, n = 5.1670)) 
+        
+        
+        neusa
+    } 
+    
+    
+}
